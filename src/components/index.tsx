a0d82f48a4be8974a0ed85e801700be181b7002a
roel127 <yalleor127@gmail.com>
2024-05-02 18:34:03 +0900

    💄 Style: layout, header 수정

---
+++ src/components/index.tsx
@@ -0,0 +1,19 @@
+import Link from 'next/link'
+
+export function Header() {
+  return (
+    <header className="text-gray-600 body-font border-b">
+      <div className="max-w-[1440px] mx-auto my-0 flex flex-wrap py-5 flex-col md:flex-row items-center justify-between">
+        <Link
+          href="/"
+          className="flex title-font font-medium items-center text-gray-900 mb-4 md:mb-0"
+        >
+          Recipe Radar
+        </Link>
+        <button className="inline-flex items-center bg-gray-100 border-0 py-1 px-3 focus:outline-none hover:bg-gray-200 rounded text-base mt-4 md:mt-0">
+          <Link href="/login">로그인/회원가입</Link>
+        </button>
+      </div>
+    </header>
+  )
+}
