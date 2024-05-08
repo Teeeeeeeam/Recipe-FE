edfbaeb5faff9f8f26d1267a898edbbdd2a1a4dc
hopak-e <a328028@gmail.com>
2024-05-08 23:51:47 +0900

    ✨ Feat : 소셜 로그인 했을 시 params로 토큰을 받아 로그인 되도록 구현

--- src/app/user/login/page.tsx
+++ src/app/user/login/page.tsx
@@ -5,14 +5,25 @@ import AuthInput from '@/components/common/auth-input'
 import { setLocalStorage } from '@/lib/local-storage'
 import Image from 'next/image'
 import Link from 'next/link'
-import { useRouter } from 'next/navigation'
-import { useState } from 'react'
+import { useRouter, useSearchParams } from 'next/navigation'
+import { useState, useEffect } from 'react'
 
 const Login = () => {
   const [username, setUsername] = useState('')
   const [password, setPassword] = useState('')
 
   const router = useRouter()
+  const params = useSearchParams()
+
+  useEffect(() => {
+    const accessToken = params.get('access-token')
+    const refreshToken = params.get('refresh-token')
+    if (accessToken && refreshToken) {
+      setLocalStorage('accessToken', accessToken)
+      setLocalStorage('refreshToken', refreshToken)
+      router.push('/')
+    }
+  }, [params])
 
   const handleLoginSubmit = async () => {
     if (!username || !password) {
@@ -32,8 +43,14 @@ const Login = () => {
 
   return (
     <div className="grow shrink px-2 space-y-2">
-      <div className="grid grid-cols-[4fr_1fr] gap-x-4 ">
-        <form className="space-y-2">
+      <form
+        className="grid grid-cols-[4fr_1fr] gap-x-4"
+        onSubmit={(e) => {
+          e.preventDefault()
+          handleLoginSubmit()
+        }}
+      >
+        <div className="space-y-2">
           <AuthInput
             type="text"
             placeholder="아이디"
@@ -46,26 +63,27 @@ const Login = () => {
             state={password}
             setState={setPassword}
           />
-        </form>
+        </div>
         <button
           type="submit"
-          onClick={handleLoginSubmit}
           className="px-2 text-sm md:text-md bg-green-100 text-white rounded-md hover:bg-green-150"
         >
           로그인
         </button>
-      </div>
+      </form>
       <div className="flex pt-3 pb-2 text-sm border-b-[1.5px] border-green-100 gap-x-1">
         <Link href="/user/join">
           <p className="hover:text-green-150">회원가입</p>
         </Link>
         <p>|</p>
-        <Link href="/user/idInquiry">
+        <Link href="/user/id-inquiry">
           <p className="hover:text-green-150">아이디 / 비밀번호 찾기</p>
         </Link>
       </div>
       <div className="flex justify-center gap-x-4">
-        <Link href="http://54.180.165.128:8080/oauth2/authorization/naver">
+        <Link
+          href={`${process.env.NEXT_PUBLIC_API_URL}/oauth2/authorization/naver`}
+        >
           <Image
             src="/naver-icon.svg"
             alt="naver-icon"
@@ -75,7 +93,9 @@ const Login = () => {
             priority
           />
         </Link>
-        <Link href="http://54.180.165.128:8080/oauth2/authorization/kakao">
+        <Link
+          href={`${process.env.NEXT_PUBLIC_API_URL}/oauth2/authorization/kakao`}
+        >
           <Image
             src="/kakao-icon.svg"
             alt="naver-icon"
