3fa41268306853236aab8d9f810e78d9f0e5d898
hopak-e <a328028@gmail.com>
2024-05-28 23:34:40 +0900

    🔧 Chore : admin관련 타입 추가

--- src/api/admin-apis.ts
+++ src/api/admin-apis.ts
@@ -1,4 +1,12 @@
-import { CookStep, Posts, Recipe, RecipeForm } from '@/types/admin'
+import {
+  Comments,
+  CookStep,
+  Members,
+  Notices,
+  Posts,
+  Recipe,
+  RecipeForm,
+} from '@/types/admin'
 import requester from './index'
 
 export const getRecipe = async (
@@ -97,9 +105,95 @@ export const deleteRecipe = async (id: number) => {
   return payload
 }
 
-export const getPosts = async () => {
+export const getPosts = async (
+  loginId: string | null,
+  recipeTitle: string | null,
+  postTitle: string | null,
+  postId: number | null,
+) => {
   const { payload } = await requester<Posts>({
     method: 'get',
-    url: '/',
+    url: `/api/search?${loginId ? `login-id=${loginId}` : ''}${recipeTitle ? `&recipe-title=${recipeTitle}` : ''}${postTitle ? `post-title=${postTitle}` : ''}${postId ? `post-id=${postId}` : ''}page=0size=10&sort=string`,
+  })
+  return payload
+}
+
+export const deletePosts = async (id: number) => {
+  const { payload } = await requester<Response>({
+    method: 'delete',
+    url: `/api/user/posts/${id}`,
+  })
+  return payload
+}
+
+export const getComments = async (id: string) => {
+  const { payload } = await requester<Comments>({
+    method: 'get',
+    url: `/api/admin/posts/comments?post-id=${id}&page=0&size=1&sort=string`,
+  })
+  return payload.data
+}
+
+export const deleteComments = async (id: string) => {
+  const { payload } = await requester<Response>({
+    method: 'delete',
+    url: ``,
+  })
+  return payload
+}
+
+export const getMembers = async (
+  loginId: string | null,
+  username: string | null,
+  email: string | null,
+  nickname: string | null,
+  memberId: number | null,
+) => {
+  const { payload } = await requester<Members>({
+    method: 'get',
+    url: `/api/admin/members/search?member-id=${memberId ? memberId : ''}${loginId ? `&login-id=${loginId}` : ''}${username ? `&username=${username}` : ''}${email ? `&email=${email}` : ''}${username ? `&username=${username}` : ''}${nickname ? `&nickname=${nickname}` : ''}&page=0&size=3&sort=string`,
+  })
+  return payload.data
+}
+
+export const deleteMembers = async (id: number[]) => {
+  const { payload } = await requester<Response>({
+    method: 'delete',
+    url: `/api/admin/members?ids=${id}`,
+  })
+  return payload
+}
+
+export const getNotice = async () => {
+  const { payload } = await requester<Notices>({
+    method: 'get',
+    url: `/api/admin/notices?page=0&size=10&sort=string`,
+  })
+  return payload.data
+}
+
+export const postNotice = async (
+  title: string,
+  content: string,
+  id: number,
+  file: File | null,
+) => {
+  const formData = new FormData()
+  file && formData.append('file', file)
+  formData.append(
+    'adminAddNoticeDto',
+    JSON.stringify({
+      noticeTitle: title,
+      noticeContent: content,
+      memberId: id,
+    }),
+  )
+  const { payload } = await requester<Response>({
+    method: 'post',
+    url: `/api/admin/notices`,
+    data: formData,
+    headers: {
+      'Content-Type': 'multipart/form-data',
+    },
   })
 }
--- src/types/admin.ts
+++ src/types/admin.ts
@@ -19,3 +19,83 @@ export interface RecipeDtoList {
   cookingTime: string
   likeCount: number
 }
+
+export interface RecipeForm extends Response {
+  data: {
+    recipe: {
+      id: number
+      imageUrl: string
+      title: string
+      cookingLevel: string
+      people: string
+      cookingTime: string
+      likeCount: number
+    }
+    ingredients: string[]
+    cookStep: CookStep[]
+  }
+}
+export interface CookStep {
+  cook_step_id?: string
+  cook_steps: string
+}
+
+export interface Posts {
+  nextPage: boolean
+  posts: PostList[]
+}
+export interface PostList {
+  id: number
+  postTitle: string
+  create_at: string
+  postImageUrl: string
+  member: { nickName: string; loginId: string }
+  recipe: { id: number; title: string }
+}
+
+export interface Comment {
+  id: number
+  comment_content: string
+  member: {
+    nickname: string
+    loginId: string
+    username: string
+  }
+  create_at: string
+}
+export interface Comments extends Response {
+  data: {
+    nextPage: false
+    comment: Comment[]
+  }
+}
+export interface MemberInfo {
+  id: number
+  username: string
+  nickname: string
+  loginId: string
+  email: string
+}
+
+export interface Members extends Response {
+  data: {
+    memberInfos: MemberInfo[]
+    nextPage: boolean
+  }
+}
+
+export interface NoticeInfo {
+  id: number
+  noticeTitle: string
+  created_at: string
+  member: {
+    nickname: string
+  }
+}
+
+export interface Notices extends Response {
+  data: {
+    nextPage: boolean
+    notice: NoticeInfo[]
+  }
+}
