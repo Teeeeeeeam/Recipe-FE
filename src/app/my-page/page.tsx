0694e6a6b762d25223461cd558668f38c37b53d3
roel127 <yalleor127@gmail.com>
2024-06-05 17:48:01 +0900

    ♻️Refactor: 마이페이지 소셜로그인 유저 다이렉트 진입

--- src/app/my-page/page.tsx
+++ src/app/my-page/page.tsx
@@ -10,6 +10,10 @@ export default function MyPage() {
   const [pw, setPw] = useState<string>('')
   const loginState = useSelector((state: RootState) => state.userInfo)
   const route = useRouter()
+  const { loginType } = loginState
+  if (loginType && loginType !== 'normal') {
+    window.location.href = '/my-page/success'
+  }
 
   async function getAccessRights(e: React.MouseEvent<HTMLButtonElement>) {
     e.preventDefault()
@@ -27,35 +31,40 @@ export default function MyPage() {
     }
   }
   return (
-    <div className="bg-slate-800 bg-opacity-50 flex justify-center items-center absolute top-0 right-0 bottom-0 left-0">
-      <div className="bg-white px-10 py-14 rounded-md text-center">
-        <p className="text-xl mb-4 font-bold text-slate-500">
-          로그인 비밀번호를 입력해주세요.
-        </p>
-        <form className="flex felx-wrap flex-col">
-          <input
-            type="password"
-            onChange={(e) => setPw(e.target.value)}
-            className="block mb-4"
-          />
-          <div>
-            <button
-              type="button"
-              onClick={() => (window.location.href = '/')}
-              className="bg-red-500 px-4 py-2 rounded-md text-md text-white"
-            >
-              Cancel
-            </button>
-            <button
-              type="submit"
-              onClick={(e) => getAccessRights(e)}
-              className="bg-indigo-500 px-7 py-2 ml-2 rounded-md text-md text-white font-semibold"
-            >
-              Ok
-            </button>
+    <>
+      {loginType === 'normal' && (
+        <div className="bg-slate-800 bg-opacity-50 flex justify-center items-center absolute top-0 right-0 bottom-0 left-0">
+          <div className="bg-white px-10 py-14 rounded-md text-center">
+            <p className="text-xl mb-4 font-bold text-slate-500">
+              로그인 비밀번호를 입력해주세요.
+            </p>
+            <form className="flex felx-wrap flex-col">
+              <input
+                type="password"
+                onChange={(e) => setPw(e.target.value)}
+                className="block mb-4"
+              />
+              <div>
+                <button
+                  type="button"
+                  onClick={() => (window.location.href = '/')}
+                  className="bg-red-500 px-4 py-2 rounded-md text-md text-white"
+                >
+                  Cancel
+                </button>
+                <button
+                  type="submit"
+                  onClick={(e) => getAccessRights(e)}
+                  className="bg-indigo-500 px-7 py-2 ml-2 rounded-md text-md text-white font-semibold"
+                >
+                  Ok
+                </button>
+              </div>
+            </form>
           </div>
-        </form>
-      </div>
-    </div>
+        </div>
+      )}
+      {loginType !== 'normal' && <div>loading</div>}
+    </>
   )
 }
