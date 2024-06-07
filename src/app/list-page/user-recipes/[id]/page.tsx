610c09f944b5aa3f545690093eece45ece8d75c7
roel127 <yalleor127@gmail.com>
2024-06-07 19:33:39 +0900

    💄Style: 모달창 렌더링 시 현재 화면에 표시

--- src/app/list-page/user-recipes/[id]/page.tsx
+++ src/app/list-page/user-recipes/[id]/page.tsx
@@ -199,7 +199,7 @@ export default function RecipeDetailUser() {
           </article>
 
           {isModal && (
-            <div className="bg-slate-800 bg-opacity-50 flex justify-center items-center absolute top-0 right-0 bottom-0 left-0">
+            <div className="bg-slate-800 bg-opacity-50 fixed flex justify-center items-center top-0 right-0 bottom-0 left-0">
               <div className="bg-white px-16 py-14 rounded-md text-center">
                 <p className="text-xl mb-4 font-bold text-slate-500">
                   비밀번호를 입력해주세요.
