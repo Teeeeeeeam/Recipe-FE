dbbd28bcd1d4a0f049918f056fb9e353e9bf9645
roel127 <yalleor127@gmail.com>
2024-06-12 23:09:40 +0900

    ✅ TEST: 2nd test

--- src/app/oauth2/login/page.tsx
+++ src/app/oauth2/login/page.tsx
@@ -5,7 +5,7 @@ import { setLocalStorage } from '@/lib/local-storage'
 import { useAppDispatch } from '@/store'
 import { getLoginInfo } from '@/store/user-info-slice'
 import { useRouter } from 'next/navigation'
-import { useEffect } from 'react'
+import { useEffect, useState } from 'react'
 
 const Link = ({
   searchParams,
@@ -16,8 +16,8 @@ const Link = ({
   const dispatch = useAppDispatch()
   const router = useRouter()
 
-  console.log('이건 const로 선언한 토큰', accessToken)
-  console.log('이건 그냥 searchParams', searchParams)
+  // console.log('이건 const로 선언한 토큰', accessToken)
+  // console.log('이건 그냥 searchParams', searchParams)
 
   const fetchLogin = async () => {
     console.log('if문 밖')
@@ -34,6 +34,33 @@ const Link = ({
     fetchLogin()
     console.log('useEffect 1번 째')
   }, [])
+
+  //
+  function useQuery() {
+    const [foo, setFoo] = useState(null)
+    if (typeof window !== 'undefined') {
+      const params = new URLSearchParams(window.location.search)
+      const queryObject: any = {}
+      params.forEach((value, key) => {
+        queryObject[key] = value
+      })
+      setFoo(queryObject)
+    }
+    return foo
+  }
+  const query: any = useQuery()
+  const token = query ? query.token : null
+
+  useEffect(() => {
+    if (token) {
+      console.log('access-token:', token)
+      // 필요한 작업 수행 (예: 토큰 저장, API 호출 등)
+    } else {
+      console.log('Token not found')
+    }
+  }, [token])
+
+  return <></>
 }
 
 export default Link
