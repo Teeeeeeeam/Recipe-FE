1ad25647f362b77daa868c7ed2186405476ffeb1
roel127 <yalleor127@gmail.com>
2024-06-12 22:34:50 +0900

    ✅ TEST: console 출력

--- src/app/oauth2/login/page.tsx
+++ src/app/oauth2/login/page.tsx
@@ -16,16 +16,23 @@ const Link = ({
   const dispatch = useAppDispatch()
   const router = useRouter()
 
+  console.log('이건 const로 선언한 토큰', accessToken)
+  console.log('이건 그냥 searchParams', searchParams)
+
   const fetchLogin = async () => {
+    console.log('if문 밖')
     if (accessToken) {
+      console.log('if문 동작함')
       setLocalStorage('accessToken', accessToken)
       const userInfo = await postUserInfo()
       dispatch(getLoginInfo(userInfo))
-      router.push('/')
+      // router.push('/')
     }
   }
   useEffect(() => {
+    console.log('useEffect 0번 째')
     fetchLogin()
+    console.log('useEffect 1번 째')
   }, [])
 }
 
