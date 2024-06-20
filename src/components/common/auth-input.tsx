07d001315e8990574b6c8b2cda8ebc798ce4ac8b
hopak-e <a328028@gmail.com>
2024-06-21 01:49:51 +0900

    ♻️ Refactor : 회원가입 리팩토링

--- src/components/common/auth-input.tsx
+++ src/components/common/auth-input.tsx
@@ -3,11 +3,12 @@ import { Dispatch, SetStateAction } from 'react'
 interface AuthInputProps {
   type: string
   placeholder: string
-  state: string
-  setState: Dispatch<SetStateAction<string>>
+  value: string
+  setValue: Dispatch<SetStateAction<string>>
   validation?: string
   id?: string
   name?: string
+
   onBlur?: (event: React.FocusEvent<HTMLInputElement>) => void
 }
 
@@ -17,8 +18,8 @@ const AuthInput = ({
   type,
   placeholder,
   validation,
-  state,
-  setState,
+  value,
+  setValue,
   onBlur,
 }: AuthInputProps) => {
   return (
@@ -27,8 +28,8 @@ const AuthInput = ({
       name={name}
       id={id}
       placeholder={placeholder}
-      value={state}
-      onChange={(e) => setState(e.target.value)}
+      value={value}
+      onChange={(e) => setValue(e.target.value)}
       onBlur={onBlur}
       className={`w-full py-2 pl-2 rounded-md ${validation && validation.length > 0 && 'border-red-50'}`}
       autoComplete="current-password"
