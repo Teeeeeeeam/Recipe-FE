73349a133a2318fb5348feea3695f389640087c9
hopak-e <a328028@gmail.com>
2024-06-21 01:49:20 +0900

    💄 Style : 사이드바 디자인 변경

--- src/lib/regexp.ts
+++ src/lib/regexp.ts
@@ -3,7 +3,7 @@ const regExp = {
   regExpPassword:
     /^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$ %^&*-]).{8,16}$/,
   regExpUsername: /^[ㄱ-ㅎ가-힣]{2,6}/,
-  regExpNickname: /^[a-zA-Z0-9ㄱ-ㅎ가-힣]{4,10}$/,
+  regExpNickname: /^[a-zA-Z0-9ㄱ-ㅎ가-힣]{4,12}$/,
   regExpEmail: /^([a-z0-9_\.-]+)@([\da-z\.-]+)\.([a-z\.]{2,6})$/,
 }
 
