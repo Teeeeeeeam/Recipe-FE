dcd94bf5d757f39a00f017d2160337765d0b5996
hopak-e <a328028@gmail.com>
2024-07-25 17:54:29 +0900

    🔧 Chore : 배포 주소 변경으로 인한 hostname 추가 및 글자 삐져나온 부분 수정

--- src/app/notices/[id]/page.tsx
+++ src/app/notices/[id]/page.tsx
@@ -23,16 +23,18 @@ const NoticeDetail = () => {
   }, [])
 
   return (
-    <div className="p-10">
+    <div className="flex p-2 md:p-10 w-full">
       {noticeInfo && (
         <div>
-          <div className="flex justify-between pb-2 border-b border-green-150">
-            <div className="flex text-[32px] font-bold">
-              <span>{noticeInfo.noticeTitle}</span>
+          <div className="grid grid-cols-[5fr_1fr] justify-between pb-2 border-b border-black break-words">
+            <div className="text-[32px] font-bold">
+              <p className="break-all">{noticeInfo.noticeTitle}</p>
             </div>
-            <div className="flex flex-col gap-x-2 text-right">
-              <span className="font-bold">{noticeInfo.member.nickname}</span>
-              <span>{noticeInfo.createdAt.slice(0, 10)}</span>
+            <div className="flex-col gap-x-2 text-right break-words">
+              <p className="font-bold break-words">
+                {noticeInfo.member.nickname}
+              </p>
+              <p>{noticeInfo.createdAt.slice(0, 10)}</p>
             </div>
           </div>
           <div className="mt-4">
@@ -47,7 +49,9 @@ const NoticeDetail = () => {
                 />
               </Link>
             )}
-            <div>{noticeInfo.noticeContent}</div>
+            <div className="break-words word-break break-all">
+              {noticeInfo.noticeContent}
+            </div>
           </div>
         </div>
       )}
